441fa89f475ed904783e7c0081f9c139eff29f66
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2022-09-30 07:37:53 -0700

    Now you can run the neural net

--- NeuralNet.py
+++ NeuralNet.py
@@ -4,35 +4,55 @@
 #ex2. input:0101, output:1010
 #ex3. input:1101, output:1101
 
-#Basic Node
-#Might get rid of this, though I may add function
-class Node:
-    def __init__(self, bias):
-        self.bias = bias
-
 #Class which contains a layer of nodes, and weights
 class Layer:
     def __init__(self, layer = 0):
         self.connectingLayer = layer
+        #This list just contains bias values
         self.nodes = []
         self.weights = []
+        #Each connecting node has its own wait(:P) list
+        if self.connectingLayer != 0:
+            for x in self.connectingLayer.nodes:
+                self.weights.append([])
     def add_node(self, node, weight):
         self.nodes.append(node)
         for x in self.weights:
             x.append(weight)
 
+#Run the neural net
+def run_net(inputVals, inputLayer):
+    #Key thing here:
+    #int(sum(values) >= bias)
+    output = []
+    #Each next layer will
+    for x in inputLayer.connectingLayer.nodes:
+        output.append(0)
+    for x in range(len(output)):
+        for y in range(len(inputVals)):
+            output[x] += inputVals[y] * inputLayer.weights[x][y]
+    for x in range(len(output)):
+        if inputLayer.connectingLayer.connectingLayer != 0:
+            output[x] = int(output[x] >= inputLayer.connectingLayer.nodes[x])
+        else:
+            return output
+    return run_net(output, inputLayer.connectingLayer)
 #Creating four Layer instances, connected to eachother
+#Also adding nodes
 OutLayer = Layer()
-Layer2 = Layer(OutLayer)
-Layer1 = Layer(Layer2)
-InLayer = Layer(Layer1)
+for x in range(4):
+    OutLayer.add_node(0.5, 1)
 
-#Adding nodes
-for x in range(4):
-    InLayer.add_node(Node(0.5) , 0)
+Layer2 = Layer(OutLayer)
 for x in range(16):
-    Layer1.add_node(Node(0.5) , 0)
+    Layer2.add_node(0.5, 0.75)
+
+Layer1 = Layer(Layer2)
 for x in range(16):
-    Layer2.add_node(Node(0.5), 0)
+    Layer1.add_node(0.5, 0.25)
+
+InLayer = Layer(Layer1)
 for x in range(4):
-    OutLayer.add_node(Node(0.5), 0)
+    InLayer.add_node(0.5, 0.25)
+
+print (run_net([1, 0, 1, 1], InLayer))
